20c8aefe555a8316ea44bb0c1762aaec936e14ed
Kerby Shedden <kshedden@umich.edu>
2022-10-31 20:24:13 -0400

    update

--- case_studies/goes/tsplot.py
+++ case_studies/goes/tsplot.py
@@ -18,23 +18,35 @@ for year in years:
     df = get_goes(year)
     df["Time"] = pd.to_datetime(df[["Year", "Month", "Day"]])
     df["DayofYear"] = [x.dayofyear for x in df.Time]
-    dx = df.groupby("DayofYear").agg({"Flux1": [np.min, np.max], "Flux2": [np.min, np.max]})
 
     for vn in ["Flux1", "Flux2"]:
-        plt.clf()
-        plt.axes([0.12, 0.12, 0.75, 0.8])
-        plt.grid(True)
-        plt.plot(dx.index, np.log10(dx[(vn, "amin")]), "-", alpha=0.5)
-        plt.plot(dx.index, np.log10(dx[(vn, "amax")]), "-", alpha=0.5)
-        plt.axhline(-6, label="C", color="green")
-        plt.axhline(-5, label="M", color="orange")
-        plt.axhline(-4, label="X", color="red")
-        ha, lb = plt.gca().get_legend_handles_labels()
-        leg = plt.figlegend(ha, lb, "center right")
-        leg.draw_frame(False)
-        plt.xlabel("Day of year", size=15)
-        plt.ylabel(vn, size=15)
-        plt.title(year)
-        pdf.savefig()
+        for dolog in [False, True]:
+
+            if dolog:
+                df["Flux"] = np.log10(df[vn])
+            else:
+                df["Flux"] = df[vn]
+            dx = df.groupby("DayofYear").agg({"Flux": [np.min, np.max]})
+
+            plt.clf()
+            plt.axes([0.15, 0.12, 0.72, 0.8])
+            plt.grid(True)
+            plt.plot(dx.index, dx[("Flux", "amin")], "-", color="blue", alpha=0.5)
+            plt.plot(dx.index, dx[("Flux", "amax")], "-", color="red", alpha=0.5)
+            if dolog:
+                plt.axhline(-6, label="C", color="green")
+                plt.axhline(-5, label="M", color="orange")
+                plt.axhline(-4, label="X", color="purple")
+            else:
+                plt.axhline(10**-6, label="C", color="green")
+                plt.axhline(10**-5, label="M", color="orange")
+                plt.axhline(10**-4, label="X", color="purple")
+            ha, lb = plt.gca().get_legend_handles_labels()
+            leg = plt.figlegend(ha, lb, "center right")
+            leg.draw_frame(False)
+            plt.xlabel("Day of year", size=15)
+            plt.ylabel(vn, size=15)
+            plt.title(year)
+            pdf.savefig()
 
 pdf.close()
